25d1ea634adb8e3c9de35285d4633d6de9b17fd8
Adithya Acharya <adithyaacharya710@gmail.com>
2024-10-22 18:00:12 +0530

    added index.js and views file with register.ejs

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const mysql = require("mysql2");
-const app = express();
+const path = require("path");
+const { faker } = require("@faker-js/faker");
 
+const app = express();
+const port = 8080;
+
+app.listen(port, ()=>{
+    console.log("listening to the port " +port);
+});
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//generating random id's
+
+let getRandomUser = () => {
+    return [
+    faker.string.uuid(),
+    ];
+  }
+
+// connection to the database
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
@@ -10,20 +30,31 @@ const connection = mysql.createConnection({
 });
 
 
-app.post("/register/hod", (req, res) => {
+app.get("/", (req, res) =>{
+    res.render("register.ejs");
 
-    let q = 'INSERT INTO hod (id, username, email, password, department ) VALUES(?)';
-    let id = 1;
+});
+
+app.use(express.urlencoded({extended: true}));
+
+//register 
+app.post("/register", (req, res) => {
+    let {user} = req.query;
+    let q = `INSERT INTO ${user} (id, name, email, password, department ) VALUES(?)`;
+    let id = getRandomUser();
+    id = id[0];
+    console.log(id);
     let {username, department, email, password, confirmPassword} = req.body;
-    let user = [id, username, email, password, department];
+
+    let data = [id, username, email, password, department];
 
     if(password == confirmPassword){
 
     try{
-        connection.query(q, [user], (err, result) =>{
+        connection.query(q, [data], (err, result) =>{
             if(err) throw err;
         
-            console.log(result);
+            res.send(result);
         });
         }
         catch(err){
