3a343f5395f09e32b199ed88d6ec42b497e72fb0
Adithya Acharya <adithyaacharya710@gmail.com>
2024-10-23 21:55:26 +0530

    added css

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.listen(port, ()=>{
     console.log("listening to the port " +port);
 });
 
+app.use(express.static("public"));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -28,21 +30,20 @@ const connection = mysql.createConnection({
     password: 'Ad2142004',
 });
 
-
 app.get("/", (req, res) =>{
-    res.render("register.ejs");
+    res.render("register-hod.ejs");
 
 });
 
 app.use(express.urlencoded({extended: true}));
 
 //register 
-app.post("/register", (req, res) => {
-    let {user} = req.query;
-    let q = `INSERT INTO ${user} (id, name, email, password, department ) VALUES(?)`;
+
+app.post("/register/hod", (req, res) => {
+
+    let q = `INSERT INTO hod (id, name, email, password, department ) VALUES(?)`;
     let id = getRandomUser();
     id = id[0];
-    console.log(id);
     let {username, department, email, password, confirmPassword} = req.body;
 
     let data = [id, username, email, password, department];
---
+++ public/register.js
@@ -0,0 +1,24 @@
+let button = document.querySelector("button");
+
+button.addEventListener("click", () =>{
+    let password = document.getElementById("password");
+    let conPassword = document.getElementById("confirmPassword");
+    let username = document.getElementById("username");
+    let department = document.getElementById("department");
+    let email = document.getElementById("email");
+
+    if(password.value != conPassword.value)
+    {   
+        let check = document.getElementById("pass-check");
+        check.innerHTML = "Please Check Your Password";
+        check.style.color="red";
+        check.style.textDecoration="underline 1px solid red";
+        event.preventDefault();
+    
+    }
+    else if(username.value && department.value && email.value){
+        document.getElementById("form").submit();
+    }
+
+    
+});
